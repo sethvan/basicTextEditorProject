cf4177e34d867cfc93dfd9cee618798d5e0d2af0
John Gibson <jg20019@gmial.com>
2022-01-12 20:16:12 -0500

    Remove unused variables

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const supBtn = document.querySelector("#sup-text");
 const undoBtn = document.querySelector("#undo");
 const redoBtn = document.querySelector("#redo");
 
-let undoCount = 0;
 
 const textBodyInnerHTMLStates = [
   {
@@ -29,7 +28,6 @@ preTag.innerText = textBody.innerHTML;
 let trend = true; //false = downwards trend and true = upwards trend
 let lengthIncrement = true; //false = downwards increment and true = upwards increment
 const keyTypeLog = [""];
-const userInputLog = [{ type: "key" }];
 const caretNodeLog = [{}];
 
 textBody.addEventListener("keyup", (e) => {
@@ -44,9 +42,6 @@ textBody.addEventListener("keyup", (e) => {
     useSpan = false;
   }
   if (textHasChanged()) {
-    userInputLog.push({
-      type: "key",
-    });
     keyTypeLog.push(`${e.key}`);
     caretNodeLog.push(getCaretNode());
     if (keyTypeLog[keyTypeLog.length - 2] === "Enter") {
@@ -291,11 +286,6 @@ const getSelectionInnerHTML = (
       textBody.innerHTML = textBodyHTML.replaceAll(myDelimiter, "");
 
       useSpan = true;
-      userInputLog.push({
-        type: "click",
-        clickType: tagType,
-        index: caretIndex,
-      });
 
       return {
         innerHTML,
@@ -492,10 +482,6 @@ const updateCurrentState = () => {
 
 const wroteWord = (keyPressed) => {
   return keyPressed === " " && keyTypeLog[keyTypeLog.length - 2] !== " ";
-};
-
-const carriageReturn = (keyPressed) => {
-  return keyPressed === "Enter";
 };
 
 const pastedOrDeletedDigits = (key) => {
